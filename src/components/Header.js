9f2f98227c775e8be376e6715a594beb3bafd9ae
Philippe Lienard <p.lienardoutlook.com@Philippes-MacBook-Pro.local>
2022-01-07 11:46:04 +0100

    svg anim

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,6 +34,13 @@ const Header = () => {
             </circle>
         </svg> */}
 
+        <h1>zerowaste.consulting</h1>
+
+        <h2>Let your marketing goals drive your technical tools</h2>
+        <p>
+            Sharp invest by your needs
+        </p>    
+
         <svg 
         className='smoothLight'
         viewBox="0 0 2098.4 815.4"
@@ -51,8 +58,17 @@ const Header = () => {
             c77.1-1.5,135.4,49.1,240.2,41.6c100.6-7.2,139.1-55.2,165-29.4c19.5,19.4-59.5,81.6-195.1,95.1c-114.9,11.4-169.1-12.1-248.5,10.2
             c-93.1,26.1-66.9,69.5-162.6,97.1c-72.6,20.9-190.8-15.5-251.9-60.4C603,408.7,563.3,331.7,578.3,316.5z"
             filter='url(#blur1)'
-            
-                />
+            >
+                {/* <animateColor 
+                attributeName="fill" 
+                attributeType="XML"
+                from="#FBB03B"
+                to="#93278F"
+                dur="6s"
+                repeatCount="indefinite"
+                /> */}
+                
+            </path>
         Sorry, your browser does not support inline SVG.
         </svg>
 
@@ -71,12 +87,7 @@ const Header = () => {
 
 
 
-        <h1>zerowaste.consulting</h1>
-
-        <h2>Let your marketing goals drive your technical tools</h2>
-        <p>
-            Sharp invest by your needs
-        </p>    
+       
     </header>
     
     return (
